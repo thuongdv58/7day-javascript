2f5a4c150d2a0f5d524a25a5395efd4c04e7f5a4
Thuongdv <thuongdv95@gmail.com>
2016-02-28 21:05:10 +0700

    image up

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>my personal site</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <img src='img/6-connect cell.png'>
+  <img src='img/Screenshot from 2016-02-28 20:21:27.png'>
+  <img src='img/Screenshot from 2016-02-28 20:21:58.png'>
+  <img src='img/Screenshot from 2016-02-28 20:36:49.png'>
+  <img src='img/Screenshot from 2016-02-28 20:36:56.png'>
+  <img src='img/Screenshot from 2016-02-28 20:37:03.png'>
+  <img src='img/Screenshot from 2016-02-28 20:37:10.png'>
+  <img src='img/Screenshot from 2016-02-28 20:45:32.png'>
+  <img src='img/Screenshot from 2016-02-28 20:45:36.png'>
+  <img src='img/Screenshot from 2016-02-28 20:45:40.png'>
+  <img src='img/Screenshot from 2016-02-28 20:45:54.png'>
+  <img src='img/1.png'>
+  <img src='img/2.png'>
+  <img src='img/3.png'>
+  <img src='img/4.png'>
+  <img src='img/5.png'>
+  <img src='img/6.png'>
+  <img src='img/7.png'>
+  <img src='img/8.png'>
+  <img src='img/9.png'>
+  <img src='img/10.png'>
+  <img src='img/11.png'>
+  <img src='img/12.png'>
+</body>
+<?php>
+  echo '<p></p>';
+</?>
+<html>
